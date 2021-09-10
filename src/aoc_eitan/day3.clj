44ac205de016184b5fd1aba89ea8fb4971e72019
Eitan Barylko <eitan@barylko.com>
2021-09-09 20:22:50 -0700

    Refactore tree-count to generate all the positions based on the slope and filter the ones that are a tree

--- src/aoc_eitan/day3.clj
+++ src/aoc_eitan/day3.clj
@@ -7,6 +7,16 @@
   (cond-> tree-count
     (= \# (nth (nth coll y) x)) inc))
 
+(defn tree?
+  "pre: takes a forest an x and y
+  post: returns true if the forest has a tree in [(mod x width) y], false otherwise"
+  [forest x y]
+  (let [width (count (first forest))]
+    (= \#
+       (nth
+        (nth forest y)
+        (mod x width)))))
+
 (defn tree-count
   "pre: takes a vector of strings, the x-coordinate of the slope and the y-coordinate of the slope
   post: returns the amount of trees encountered by applying a slope of -1/3 repeatedly until crossing the final row"
@@ -17,6 +27,25 @@
       (recur (check-tree coll tree x y)
              (mod (+ x x-slope) (count (first coll)))
              (+ y y-slope)))))
+
+(defn inside?
+  [forest [_ row]]
+  (< row (count forest)))
+
+(defn slope->pos
+  "pre: takes a slop and a forest
+  post: a infinite lazy collection of all the positions starting from [0 0] and adding the slope repeatedly"
+  [slope]
+  (iterate #(map + slope %) [0 0]))
+
+
+(defn tree-count
+  [forest x y]
+  (->> (slope->pos [x y])
+       (take-while (partial inside? forest))
+       (filter #(apply tree? forest %))
+       count))
+
 
 (defn many-slopes
   "pre: takes a collection of slopes and a forest
