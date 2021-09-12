d3865e0a896ff5b3e12fb1783042afea83169955
Eitan Barylko <eitan@barylko.com>
2021-09-11 18:15:56 -0700

    removed the version of check-tree that used a loop

--- src/aoc_eitan/day3.clj
+++ src/aoc_eitan/day3.clj
@@ -1,11 +1,5 @@
 (ns aoc-eitan.day3)
 
-(defn check-tree
-  "pre: takes an x and y coordinate of the forest, the forest, and a count of the trees encountered
-  post: if the x and y coordinate at the forest contains '#', then increment the value of the tree count, otherwise add nothing."
-  [coll tree-count x y]
-  (cond-> tree-count
-    (= \# (nth (nth coll y) x)) inc))
 
 (defn tree?
   "pre: takes a forest an x and y
@@ -17,23 +11,12 @@
         (nth forest y)
         (mod x width)))))
 
-(defn tree-count
-  "pre: takes a vector of strings, the x-coordinate of the slope and the y-coordinate of the slope
-  post: returns the amount of trees encountered by applying a slope of -1/3 repeatedly until crossing the final row"
-  [coll x-slope y-slope]
-  (loop [tree 0 x 0 y 0]
-    (if (>= y (count coll))
-      tree
-      (recur (check-tree coll tree x y)
-             (mod (+ x x-slope) (count (first coll)))
-             (+ y y-slope)))))
-
 (defn inside?
   [forest [_ row]]
   (< row (count forest)))
 
 (defn slope->pos
-  "pre: takes a slop and a forest
+  "pre: takes a slope and a forest
   post: a infinite lazy collection of all the positions starting from [0 0] and adding the slope repeatedly"
   [slope]
   (iterate #(map + slope %) [0 0]))
